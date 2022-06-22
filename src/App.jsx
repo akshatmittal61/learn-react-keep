a054b552fc5877d6af18c236c8a402c791ea22db
Akshat Mittal <akshatmittal2506@gmail.com>
2022-06-22 21:53:23 +0530

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import './style.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Header />
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <span>Keep Notes</span>
+    </header>
+  )
+}
+
+export default Header
